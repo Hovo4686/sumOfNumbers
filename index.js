282892bfad79eb8fdc76c952a1ae2bf80dc0717a
Hovo4686 <131679116+Hovo4686@users.noreply.github.com>
2023-07-19 18:30:58 +0400

    find sum of numbers with  1 for loop

---
+++ index.js
@@ -0,0 +1,17 @@
+
+    const checkTargetIndex = (arr, target) => {
+        const indices = {};
+        for (let i = 0; i < arr.length; i++) {
+        const complement = target - arr[i];
+        if (indices.hasOwnProperty(complement)) {
+        return [indices[complement], i];
+        }
+        indices[arr[i]] = i;
+       }
+       return null; // No two numbers found whose sum is equal to the target
+     };
+    console.log(checkTargetIndex([1, 2, 3, 4, 5], 9));
+
+// change number in console and return which numbers of arr  sum
+
+
